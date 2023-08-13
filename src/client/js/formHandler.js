a014550597936f17a432ced7fc8c326789c889fb
nttnguyen <nttnguyen136@gmail.com>
2023-08-13 14:25:48 +0700

    update project

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -17,13 +17,26 @@ function handleSubmit(event) {
 
   let urlValue = document.getElementById("url").value;
 
-  console.log("::: Form Submitted :::");
+  if (!urlValue) {
+    alert("Url can not empty!");
+    return;
+  }
 
-  postData("http://localhost:8088/url", {
+  postData("http://localhost:8080/url", {
     url: urlValue,
   }).then((res) => {
-    document.getElementById("results").innerHTML = res.message;
+    // Get only basic info
+    const resValue = {
+      agreement: res.agreement,
+      confidence: res.confidence,
+      irony: res.irony,
+      model: res.model,
+      score_tag: res.score_tag,
+      subjectivity: res.subjectivity,
+    };
+
+    document.getElementById("results").innerHTML = JSON.stringify(resValue);
   });
 }
 
-export { handleSubmit, onBlur };
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,26 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-var path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const cors = require("cors");
+
+const mockAPIResponse = require("./mockAPI.js");
 
 const API_KEY = process.env.API_KEY;
 const URL = "https://api.meaningcloud.com/sentiment-2.1?";
 
 console.log(`Your API key is ${API_KEY}`);
 
-const mockAPIResponse = require("./mockAPI.js");
-
+const PORT = 8080;
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("dist"));
+app.use(cors());
 
-console.log(__dirname);
+app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
@@ -34,10 +32,8 @@ app.get("/test", function (req, res) {
 });
 
 // POST Route
-app.post("/url", async function (req, res) {
+app.post("/url", async (req, res) => {
   let url = req.body.url;
-
-  console.log(`Url: ${url}`);
 
   const input = `${URL}key=${API_KEY}&url=${url}&lang=en`;
 
@@ -49,6 +45,7 @@ app.post("/url", async function (req, res) {
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8088, function () {
-  console.log("Example app listening on port 8088!");
+app.listen(PORT, () => {
+  console.log("Server running...");
+  console.log(`Running on localhost: ${PORT}`);
 });
